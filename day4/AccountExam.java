81b9b6e7c782bc62a1e35a4d31ef7de8930fbea4
orderabbit <dur369667@naver.com>
2024-03-07 10:15:40 +0900

    java

--- day4/AccountExam.java
+++ day4/AccountExam.java
@@ -2,7 +2,7 @@ package day4;
 
 public class AccountExam {
     public static void main(String[] args) {
-        Account a1 = new Account();
+        Account a1 = new Account(); // 철수 계좌
 
         a1.deposit(1000); // 1000원을 예금했습니다.
         a1.deposit(2000); // 2000원을 예금했습니다.
@@ -19,6 +19,15 @@ public class AccountExam {
 
         remainder = a1.getRemainder();
         System.out.println(remainder); // 4200
+
+        Account a2 = new Account(); // 영희 계좌
+        a2.deposit(1000);
+
+        remainder = a1.getRemainder(); // 철수 계좌 잔액
+        System.out.println(remainder);
+
+        int remainder2 = a2.getRemainder(); // 영희 계좌 잔액
+        System.out.println(remainder2);
     }
 }
 
